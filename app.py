fee90289c4cf8bcaff7514b457a688ace3b006fc
qingfeng <qingfeng@douban.com>
2012-11-24 23:09:50 +0800

    把APIKEY存到config

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from flask import render_template as st
 
+from douban_client import DoubanClient
+
+API_KEY = os.environ['API_KEY']
+API_SECRET = os.environ['API_SECRET']
+
+SCOPE = 'douban_basic_common,shuo_basic_r,shuo_basic_w'
+
+client = DoubanClient(API_KEY, API_SECRET, '/back', SCOPE)
+
 app = Flask(__name__)
 
 @app.route('/login/')
 def login():
+    auth_url = client.authorize_url
     return st('login.html', **locals())
 
 @app.route('/back')
@@ -16,22 +26,11 @@ if __name__ == '__main__':
     port = 5000
     app.run(host='0.0.0.0', port=port, debug=True)
 
-'''
-from douban_client import DoubanClient
 
-API_KEY = 'your api key'
-API_SECRET = 'your api secret'
+#print 'Go to the following link in your browser:'
+#code = raw_input('Enter the verification code:')
+#client.auth_with_code(code)
 
-SCOPE = 'douban_basic_common,shuo_basic_r,shuo_basic_w'
+#client.auth_with_token(token)
 
-client = DoubanClient(API_KEY, API_SECRET, your_redirect_uri, SCOPE)
-
-print 'Go to the following link in your browser:'
-print client.authorize_url
-code = raw_input('Enter the verification code:')
-client.auth_with_code(code)
-
-client.auth_with_token(token)
-
-new miniblog client.miniblog.new(text)
-'''
+#new miniblog: client.miniblog.new(text)
