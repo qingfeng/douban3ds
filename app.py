917c1bd867f33e4e841321cb1fc3325b9361274e
qingfeng <qingfeng@douban.com>
2012-11-25 11:39:14 +0800

    模板重新整理,增加了发图的界面和功能初步

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask import request
 from flask import render_template as st
 
 from douban_client import DoubanClient
+
+import os
 
 API_KEY = os.environ['API_KEY']
 API_SECRET = os.environ['API_SECRET']
@@ -19,18 +22,23 @@ def login():
 
 @app.route('/back')
 def back():
-    return 'Back'
+    code = request.args.get('code', '')
+    error = request.args.get('error', '')
+    if code:
+        return st('index.html', **locals())
+    elif error:
+        return error
+    else:
+        return 'Unkonw Error'
 
+@app.route('/new', methods=['POST'])
+def new():
+    code = request.form.get('code')
+    image = request.files.get('image')
+    client.auth_with_code(code)
+    client.miniblog.new(text, image=image)
+    return "Upload OK"
 
 if __name__ == '__main__':
     port = 5000
     app.run(host='0.0.0.0', port=port, debug=True)
-
-
-#print 'Go to the following link in your browser:'
-#code = raw_input('Enter the verification code:')
-#client.auth_with_code(code)
-
-#client.auth_with_token(token)
-
-#new miniblog: client.miniblog.new(text)
