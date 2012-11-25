85f5c25632a6e1e9e9ebfa5c87a38024cf49a840
qingfeng <qingfeng@douban.com>
2012-11-25 18:36:34 +0800

    add url callback and post image to miniblog

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
 from flask import Flask
-from flask import request
+from flask import request, redirect
 from flask import render_template as st
 
 from douban_client import DoubanClient
+from douban_client.api.error import DoubanError
 
 import os
 
 API_KEY = os.environ['API_KEY']
 API_SECRET = os.environ['API_SECRET']
-
+CALLBACK = os.environ['API_CALLBACK']
 SCOPE = 'douban_basic_common,shuo_basic_r,shuo_basic_w'
 
-client = DoubanClient(API_KEY, API_SECRET, '/back', SCOPE)
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/login/')
 def login():
+    client = DoubanClient(API_KEY, API_SECRET, CALLBACK, SCOPE)
     auth_url = client.authorize_url
     return st('login.html', **locals())
 
@@ -33,12 +41,18 @@ def back():
 
 @app.route('/new', methods=['POST'])
 def new():
+    client = DoubanClient(API_KEY, API_SECRET, CALLBACK, SCOPE)
     code = request.form.get('code')
     image = request.files.get('image')
     text = request.form.get('comment', '')
     client.auth_with_code(code)
-    #client.miniblog.new(text, image=image)
-    client.miniblog.new(text)
+    if image and allowed_file(image.filename):
+        from cStringIO import StringIO
+        image.save('/tmp/1.jpg')
+        try:
+            client.miniblog.new(text, image=open('/tmp/1.jpg'))
+        except DoubanError:
+            return redirect('/login')
     return "Upload OK"
 
 if __name__ == '__main__':
